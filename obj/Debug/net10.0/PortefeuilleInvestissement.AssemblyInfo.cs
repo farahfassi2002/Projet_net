bfb9aec114cbe027514c3fbf92edb2e87569ed90
farahfassi2002 <farah.fassi@polytechnicien.tn>
2026-05-22 12:58:13 +0100

    done

--- obj/Debug/net10.0/PortefeuilleInvestissement.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortefeuilleInvestissement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortefeuilleInvestissement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe55aa53420749893cb6e834c289606a4e1c7a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac150b1c3ec6bf2a7615c257351611a51437185")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortefeuilleInvestissement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortefeuilleInvestissement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
